e7a39418e80904d9fd7f07c4c64db372dbc28827
Lior Kovalio <lior.kovalio@gmail.com>
2023-03-13 21:14:16 +0200

    removed server logic for debugging

--- src/routes/api/pusher/auth/+server.js
+++ src/routes/api/pusher/auth/+server.js
@@ -1,6 +1,6 @@
 import Pusher from "pusher";
 
-let waitingList = [];
+// let waitingList = [];
 
 export const pusher = new Pusher({
   // connect to pusher
@@ -11,39 +11,44 @@ export const pusher = new Pusher({
 });
 
 export default async function POST(req, res) {
-    const username = req.body.username;
+    // const username = req.body.username;
+    const socketId = req.body.socket_id;
+    const channel = req.body.channel_name;
+    console.log(socketId, channel);
+    const auth = pusher.authorizeChannel(socketId, channel);
+    res.send(auth);
 
-    if (waitingList.indexOf(username) === -1) {
-      waitingList.push(username);
-      console.log("connecting", username);
+    // if (waitingList.indexOf(username) === -1) {
+    //   waitingList.push(username);
+    //   console.log("connecting", username);
   
-      if (waitingList.length >= 2) {
-        let p1, p2;
-        [p1, p2, ...waitingList] = waitingList;
-        console.log(`pairing up {${p1} , ${p2}}`);
+    //   if (waitingList.length >= 2) {
+    //     let p1, p2;
+    //     [p1, p2, ...waitingList] = waitingList;
+    //     console.log(`pairing up {${p1} , ${p2}}`);
   
-        // trigger a message to player one and player two on their own channels
-        await pusher.trigger(
-          ["private-user-" + p1, "private-user-" + p2],
-          "paired",
-          {
-            players: [p1, p2],
-          }
-        );
-      } else {
-        await pusher.trigger(
-          ["private-user-" + username],
-          "waiting_for_opponent",
-          {}
-        );
-      }
+    //     // trigger a message to player one and player two on their own channels
+    //     await pusher.trigger(
+    //       ["private-user-" + p1, "private-user-" + p2],
+    //       "paired",
+    //       {
+    //         players: [p1, p2],
+    //       }
+    //     );
+    //   } else {
+    //     await pusher.trigger(
+    //       ["private-user-" + username],
+    //       "waiting_for_opponent",
+    //       {}
+    //     );
+    //   }
   
-      const socketId = req.body.socket_id;
-      const channel = req.body.channel_name;
-      console.log(socketId, channel);
-      const auth = pusher.authorizeChannel(socketId, channel);
-      res.send(auth);
-    } else {
-      res.status(400);
-    }
+    //   const socketId = req.body.socket_id;
+    //   const channel = req.body.channel_name;
+    //   console.log(socketId, channel);
+    //   const auth = pusher.authorizeChannel(socketId, channel);
+    //   res.send(auth);
+    // } else {
+    //   res.status(400);
+    // }
 }
