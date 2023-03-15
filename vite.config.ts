5bfd8cc7dfc4d066d1a58185519cfbb7fdb58f6a
Lior Kovalio <lior.kovalio@gmail.com>
2023-03-15 12:26:11 +0200

    remove debug pre on production

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	define: {
+		__APP_ENV__: process.env.VITE_VERCEL_ENV,
+	},
 });
